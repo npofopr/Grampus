414b1ff93f949392970ce0498c8b577ace9dad76
npofopr <npofopr@gmail.com>
2020-03-04 18:52:24 +0500

    fix slider

--- js/index.js
+++ js/index.js
@@ -1,6 +1,8 @@
 $(document).ready(function () {
   var mySwiper = new Swiper ('.slider', {
     // Optional parameters
-    loop: true
+    loop: true,
+    slidesPerView: 3,
+    spaceBetween: 30
   })
 });
