ee3b4c38851c56e2e80008bff0b17e1d35759c8f
npofopr <npofopr@gmail.com>
2020-03-13 21:42:56 +0500

    13, 14 уроки

--- js/index.js
+++ js/index.js
@@ -1,8 +1,86 @@
+$(function() {
+
+  // Preloader
+  $(window).on( 'load', function() {
+    setTimeout(function() {
+      $('body').addClass('is-load');
+    }, 100);
+  });
+
+  // Scroll Header
+  $(window).on( 'load scroll touchmove', function() {
+    var sticky = $('body'),
+      scroll = $(window).scrollTop();
+    if (scroll >= 300) sticky.addClass('is-fixed');
+      else sticky.removeClass('is-fixed');
+  });
+
+  // $(window).on( 'resize', function() {
+  // 	$('body').removeClass('is-visible');
+  // });
+
+  $('.hamburger-menu').on( 'click', function(event) {
+    if( $(this).is('.is-active')) {
+      $(this).removeClass('is-active').closest('body').removeClass('is-visible');
+    }
+    else {
+      $(this).addClass('is-active').closest('body').addClass('is-visible');
+    }
+    event.preventDefault();
+  });
+
+  $('.close, .offcanvas__overlay').on( 'click', function(event) {
+    $('body').removeClass('is-visible');
+    $('.hamburger-menu').removeClass('is-active')
+    event.preventDefault();
+  });
+
+  // let linkAuth = document.querySelector('.link-auth');
+  // let modal = document.querySelector('.modal');
+
+  // linkAuth.addEventListener('click', function (e) {
+  //   e.preventDefault();
+  //   console.log('клик');
+  //   modal.classList.add('modal-show');
+  // });
+
+  // window.addEventListener('keydown', function (e) {
+  //   if (e.keyCode === 27) {
+  //     if (modal.classList.contains('modal-show')) {
+  //       e.preventDefault();
+  //       modal.classList.remove('modal-show');
+  //     }
+  //   }
+  // });
+
+});
+
+
 $(document).ready(function () {
-  var mySwiper = new Swiper ('.slider', {
+  var mySwiper = new Swiper('.slider', {
     // Optional parameters
     loop: true,
-    slidesPerView: 3,
-    spaceBetween: 30
+    slidesPerView: 1,
+    spaceBetween: 10,
+    breakpoints: {
+      // when window width is >= 480px
+      480: {
+        slidesPerView: 2,
+        spaceBetween: 30
+      },
+      // when window width is >= 640px
+      640: {
+        slidesPerView: 3,
+        spaceBetween: 40
+      }
+    }
   })
 });
+
+// (function($) {
+//   $(function() {
+
+//     $('select').styler();
+
+//   });
+//   })(jQuery);
