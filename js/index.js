8b048715c01c0349e3487c95802d2d6788c9e64e
npofopr <npofopr@gmail.com>
2020-03-16 14:48:29 +0500

    добавил скрипт прокрутки вверх

--- js/index.js
+++ js/index.js
@@ -1,26 +1,38 @@
-$(function() {
+$(function () {
 
   // Preloader
-  $(window).on( 'load', function() {
-    setTimeout(function() {
+  $(window).on('load', function () {
+    setTimeout(function () {
       $('body').addClass('is-load');
     }, 100);
   });
 
   // Scroll Header
-  $(window).on( 'load scroll touchmove', function() {
+  $(window).on('load scroll touchmove', function () {
     var sticky = $('body'),
       scroll = $(window).scrollTop();
     if (scroll >= 300) sticky.addClass('is-fixed');
-      else sticky.removeClass('is-fixed');
+    else sticky.removeClass('is-fixed');
+  });
+
+  // Scroll to id
+  $('.scroll_link[href^="#"]').bind('click.smoothscroll', function (e) {
+    e.preventDefault();
+    var target = this.hash,
+      $target = $(target);
+    $('html, body').stop().animate({
+      'scrollTop': $target.offset().top
+    }, 900, 'swing', function () {
+      window.location.hash = target;
+    });
   });
 
   // $(window).on( 'resize', function() {
   // 	$('body').removeClass('is-visible');
   // });
 
-  $('.hamburger-menu').on( 'click', function(event) {
-    if( $(this).is('.is-active')) {
+  $('.hamburger-menu').on('click', function (event) {
+    if ($(this).is('.is-active')) {
       $(this).removeClass('is-active').closest('body').removeClass('is-visible');
     }
     else {
@@ -29,7 +41,7 @@ $(function() {
     event.preventDefault();
   });
 
-  $('.close, .offcanvas__overlay').on( 'click', function(event) {
+  $('.close, .offcanvas__overlay').on('click', function (event) {
     $('body').removeClass('is-visible');
     $('.hamburger-menu').removeClass('is-active')
     event.preventDefault();
@@ -79,8 +91,6 @@ $(document).ready(function () {
 
 // (function($) {
 //   $(function() {
-
 //     $('select').styler();
-
 //   });
 //   })(jQuery);
