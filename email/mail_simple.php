cf01c4290084be113a4b839a5f222477b0499757
npofopr <npofopr@gmail.com>
2020-03-18 18:44:04 +0500

    upd mail

--- email/mail_simple.php
+++ email/mail_simple.php
@@ -8,14 +8,13 @@
       <title>' . $subject . '</title>
     </head>
     <body>
-      <p>Заявка: '.$_POST['subject'].'</p>
-      <p>Телефон: '.$_POST['tel'].'</p>
-      <p>Email: '.$_POST['email'].'</p>
+      <p>Заявка: ' . $_POST['subject'] . '</p>
+      <p>Телефон: ' . $_POST['tel'] . '</p>
+      <p>Email: ' . $_POST['email'] . '</p>
     </body>
   </html>';
 
   $header = "Content-Type: text/html; charset=utf-8 \r\n";
-  $header = "From: Отправитель <".$to.">\r\n";
-
+  $header .= "From: Отправитель <".$to.">\r\n";
   mail($to, $subject, $message, $header);
-?>
+?>1
--- js/mail_simple.js
+++ js/mail_simple.js
@@ -1,17 +1,20 @@
-$(document).ready(function () {
+$(document).ready(function() {
 
-  var th = $(this);
-  $.ajax({
-    type: "POST",
-    url: "/email/mail_simple.php", //Change
-    data: th.serialize()
-  }).done(function () {
-    console.log('Thank you!');
-
-    setTimeout(function () {
-      // Done Functions
-      th.trigger("reset");
-    }, 1000);
-  });
+	//E-mail Ajax Send
+	$(".form-feedback").submit(function() { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "./email/mail_simple.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			alert("Thank you!");
+			setTimeout(function() {
+				// Done Functions
+				th.trigger("reset");
+			}, 1000);
+		});
+		return false;
+	});
 
 });
